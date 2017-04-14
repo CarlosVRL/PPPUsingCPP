eb72e186292e66af2ca9f0e47bdbb92288d12f93
Carlos Varela <carlos.varela.eng@gmail.com>
2017-04-14 18:12:09 -0500

    Added i/o example

---
+++ Ch_3/inputrw.cpp
@@ -0,0 +1,12 @@
+// Training Library
+#include "../lib/std_lib_facilities.h"
+
+int main() {
+    
+    // Prompt user to enter name
+    cout << "Please enter your first name: \n";
+    string first_name;
+    cin >> first_name;
+    
+    return 0;
+}
