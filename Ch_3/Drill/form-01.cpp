52813e95164f776d23e08a8c60d3d0f30614c412
Carlos Varela <carlos.varela.eng@gmail.com>
2017-04-14 23:53:28 -0500

    Step 4

--- Ch_3/Drill/form-01.cpp
+++ Ch_3/Drill/form-01.cpp
@@ -40,8 +40,27 @@ int main()
     cin >> friend_name;
     
     // Reference to other friend
-    cout << "\nHave you seen " << friend_name << " lately?\n";
+    cout << "\nHave you seen " << friend_name << " lately?\n\n";
     
+    // Friend gender
+    char friend_sex = 0;
+    cout << "Enter an 'm' for male or 'f' for female: ";
+    cin >> friend_sex;
+    cout << endl;
+    
+    // Follow up message based on gender
+    switch(friend_sex)
+    {
+        case 'm' :
+            cout << "If you see " << friend_name << ", please ask HIM to call me.\n\n";
+            break;
+        case 'f' :
+            cout << "If you see " << friend_name << ", please ask HER to call me.\n\n";
+            break;
+        default :
+            cout << "If you see " << friend_name << ", please ask THEM to call me.\n\n";
+            break;
+    }
     // Successful return
     return 0;
 }
