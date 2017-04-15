4d5fe7e3058573615dfdf0b08998a2a3f8d372e9
Carlos Varela <carlos.varela.eng@gmail.com>
2017-04-14 20:56:02 -0500

    Reorganizing Directories

--- Ch_3/inputrw.cpp
+++ Ch_3/inputrw.cpp
@@ -22,3 +22,28 @@ int main()
 
     return 0;
 }
+
+/*
+ * Drill 2
+ *
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * /
+
+#include "../lib/std_lib_facilities.h"
+
+int main()
+{
+    cout << "Please enter your first name and age\n";
+    string first_name;
+    int age;
+    
+    cin >> first_name;
+    cin >> age;
+    
+    cout << "Hello, " << first_name << " (age "<< age << ")\n";
+    
+    return 0;
+}
+ */
