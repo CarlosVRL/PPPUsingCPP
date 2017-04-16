34159a703f79d850624150c7b883622f75d9c34a
Carlos Varela <carlos.varela.eng@gmail.com>
2017-04-16 17:19:27 -0500

    Step 2

--- Ch_4/Drill/compare-01.cpp
+++ Ch_4/Drill/compare-01.cpp
@@ -5,42 +5,52 @@
  * Created on April 16, 2017, 1:32 PM
  */
 
-#include <iostream>
-#include <vector>
+#include <iostream>     // std::cout, cin
+#include <algorithm>    // std::sort
+#include <vector>       // std::vector
 
 using namespace std;
 
 int main()
 {
-    // Task 1: Write a program that consists of a while-loop that
-    // (each time around the loop) reads in two ints and then
-    // prints them. Exit the program when a terminating '|' is
-    // entered
+    // Task 2: Change the program to write out the smaller value is:
+    // followed by the smaller of the numbers and the larger value is:
+    // followed by the larger value
     
     // Initialize empty array
     vector<int> numbers;
     
-    // Temporary integer container
+    // Temporary integer container (from input)
     int temp;
-    string go = "";
     
-    // Size of array
-    int numbers_size;
+    // While loop conditional variable
+    string go = "";
     
     // Reads input until any non-integer is entered (|)
     while (go != "|")
     {
-        // Read integer to local vector
+        // Read integer to vector
         cout << "Give me an int: ";
         cin >> temp;
         numbers.push_back(temp); // use push back instead
         
+        // Read another integer to vector
         cout << "Give me another int: ";
         cin >> temp;
         numbers.push_back(temp);
         
         // Print both input integers to screen
         cout << "You gave me: " << numbers[numbers.size()-2] << " and " << numbers.back() << endl;
+        
+        // Sort the vector and print sorted
+        sort(numbers.begin(),numbers.end());
+        
+        // Case: Values are relative
+        cout << "the smaller value us: " << numbers[0] << endl;
+        cout << "the larger value is: " << numbers[1] << endl;
+        
+        // Clear vector for next loop
+        numbers.clear();
         
         // Recursion
         cout << "Enter '|' to exit, or enter any other character to continue: ";
